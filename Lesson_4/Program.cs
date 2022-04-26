42fb2324160fd75c628d71b25606bb930e77da30
Natali <fonatka@gmail.com>
2022-04-26 17:42:15 +0300

    Lesson 4 and 5 added

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,115 @@
+﻿//Задача 1. Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+/*
+int NaturalSum(int a)
+{
+    int sum = 0;
+
+
+    if (a < 1) return 0;
+    else
+    {
+        for (int i = 1; i <= a; i++)
+        {
+            sum = sum + i; // sum += i; -= *= и т д то эже самое, упрощенная запись
+        }
+        return sum;
+    }
+}
+Console.Write("Input number A ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Sum of elements is " + NaturalSum(num));
+*/
+
+// Задача 2. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
+void CountNum (int num1)
+{
+    int count = 0;
+    while (num1 != 0)
+    {
+        num1 /= 10;
+        count++;
+    }
+    Console.Write(count);
+}
+Console.Write("Input Number ");
+int num = Convert.ToInt32(Console.ReadLine());
+CountNum(num);
+*/
+
+// Задача 3. Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*
+int Factorial(int a)
+{
+    int fact = 1;
+
+    if (a < 1) return 0;
+    else
+    {
+        for (int i = 1; i <= a; i++)
+        {
+            fact = fact * i; 
+        }
+        return fact;
+    }
+}
+Console.Write("Input number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Factorial " + Factorial(num));
+*/
+// Задача 4. Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке. [1,0,1,1,0,1,0,0]
+/*
+void PrintArray()
+{
+    int[] ar = new int[8];
+
+    for (int i = 0; i < ar.Length; i++)
+    {
+        ar[i] = new Random().Next(0, 2);
+        Console.Write(ar[i] + " ");
+    }
+}
+PrintArray();
+*/
+// Задача 5. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+
+int[] CreateRandomArray(int size, int min, int max) // int min, int max - для рандомных чисел
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        //Console.Write("Input " + i + " element: ");
+        //array[i] = Convert.ToInt32(Console.ReadLine());
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+void FindSumOfElements(int[] array)
+{
+    int plusSum = 0;
+    int minusSum = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            plusSum += array[i];
+        else
+            minusSum += array[i];
+    }
+    Console.WriteLine("Sum of positive elements is " + plusSum);
+    Console.WriteLine("Sum of negative elements is " + minusSum);
+}
+
+int[] myArray = CreateRandomArray(12, -9, 9);
+ShowArray(myArray);
+Console.WriteLine();
+FindSumOfElements(myArray);
---
+++ Lesson_5/Program.cs
@@ -0,0 +1,162 @@
+﻿/*int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+*/
+// Задача 1. Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие 
+//отрицательные, и наоборот.
+
+
+/*
+int[] ChangeNums(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    array[i] *= (-1));
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+int[] myArray = CreateRandomArray(12, -9, 9);
+ShowArray(myArray);
+ChangeNums(myArray);
+*/
+
+
+
+
+
+//Задача 2. Задайте массив. Напишите программу, 
+//которая определяет, присутствует ли заданное число 
+//в массиве.
+/*
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+string FindNumber(int[] array, int numbers)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == numbers)
+        {
+            return "Your number in array";
+        }
+    }
+    return "Your number NOT in array";
+
+}
+Console.Write("Input number: ");
+int Num = Convert.ToInt32(Console.ReadLine());
+int[] myArray = CreateArray(5, 0, 5);
+ShowArray(myArray);
+Console.WriteLine();
+Console.WriteLine(FindNumber(myArray, Num));
+*/
+
+// Задача 3. Задайте одномерный массив из 15 случайных 
+//чисел. Найдите количество элементов массива, значения 
+//которых лежат в отрезке [10,99].
+/*
+int[] CreateArray()
+{
+    int[] array = new int[15];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+        Console.Write(array[i] + " ");
+    }
+    return array;
+}
+*/
+/*
+void CountElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100)
+        count++;
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Количество элементов, которые лежат в отрезке от 10 до 100 = {count}");
+}
+*/
+/*
+int CountElements(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 9 && array[i] < 100)
+            count++;
+    }
+    return count;
+
+}
+
+CountElements(CreateArray());
+*/
+// Задача 4. Найдите произведение пар чисел в одномерном 
+//массиве. Парой считаем первый и последний элемент, 
+//второй и предпоследний и т.д. Результат запишите в 
+//новом массиве.
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+}
+
+int[] NewMagicArray(int[] array)
+{
+    int newSize = array.Length / 2;
+    int[] newArray = new int[newSize];
+
+    for (int i = 0, j = array.Length - 1; i < newSize; i++, j--)
+    {
+        newArray[i] = array[i] * array[j];
+    }
+    return newArray;
+
+}
+
+int[] myArray = CreateArray(7, 1, 6);
+int[] newArr = NewMagicArray(myArray);
+ShowArray(myArray);
+Console.WriteLine();
+ShowArray(newArr);
+
+
