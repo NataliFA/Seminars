291bd60d39a83d401d768f9cb5aa59c5db3c442c
Natali <fonatka@gmail.com>
2022-05-12 14:13:11 +0300

    Lesson_1 checked and corrected

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Imput integer number: ");
 int number = Convert.ToInt32(Console.ReadLine()); // преобразует целочисленное значение 25, "25"- это строка
 
-int Result = number*number;
+int Result = number * number;
 
 Console.WriteLine("Result is " + Result);
 */
@@ -13,14 +13,12 @@ Console.WriteLine("Result is " + Result);
 Console.Write("Imput first integer number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Imput second integer number: ");
-int num2 = Convert.ToInt32(Console.ReadLine()); 
+int num2 = Convert.ToInt32(Console.ReadLine());
 
 //int quad = num2*num2; можно убрать эту команду, можно убрать фигурные скобки, так как всего по 1 строке в условии
 
-if (num1 == num2*num2)
-    Console.WriteLine("First number is a quad of second number");
-else
-    Console.WriteLine("First number is not a quad of second number");
+if (num1 == num2 * num2) Console.WriteLine("First number is a quad of second number");
+else Console.WriteLine("First number is NOT a quad of second number");
 */
 
 //Задача 3. Напишите программу, которая преобразует число в день недели. 
@@ -32,20 +30,13 @@ if (day > 7 || day < 1)
     Console.WriteLine("Incorrect number of day!");
 else
 {
-    if (day == 1)
-        Console.WriteLine("It's Monday");
-    if (day == 2)
-        Console.WriteLine("It's Thuesday");
-    if (day == 3)
-        Console.WriteLine("It's Wednesday");
-    if (day == 4)
-        Console.WriteLine("It's Thursday");
-    if (day == 5)
-        Console.WriteLine("It's Friday");
-    if (day == 6)
-        Console.WriteLine("It's Saturday");
-    if (day == 7)
-        Console.WriteLine("It's Sunday");
+    if (day == 1) Console.WriteLine("It's Monday");
+    if (day == 2) Console.WriteLine("It's Thuesday");
+    if (day == 3) Console.WriteLine("It's Wednesday");
+    if (day == 4) Console.WriteLine("It's Thursday");
+    if (day == 5) Console.WriteLine("It's Friday");
+    if (day == 6) Console.WriteLine("It's Saturday");
+    if (day == 7) Console.WriteLine("It's Sunday");
 }
 */
 
@@ -60,12 +51,9 @@ if (num < 0)
     current = num;
     num = num * (-1);
 }
-else
-{
-    current = -1 * num;
-}
+else current = -1 * num;
 
-while(current <= num)
+while (current <= num)
 {
     Console.Write(current + " "); // здесь просто пробел между числами " "
     current++;
@@ -77,14 +65,11 @@ while(current <= num)
 Console.Write("Imput integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num < 100 || num > 999)
-{
-   Console.WriteLine("Incorrect number");
-}
+if (num < 100 || num > 999) Console.WriteLine("Incorrect number");
 else
 {
     int a = num % 10;
-    Console.WriteLine("Number is " + a);
+    Console.WriteLine("The last digit of the number " + a);
 }
 */
 
